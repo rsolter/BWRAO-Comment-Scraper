688f515906686da683d5573c9319967bb081c725
Ravi Solter <ravisolter@gmail.com>
2020-04-11 23:12:44 -0400

    second mass commit - reorganizing, consolidating comments into 6 files

--- BWRAO Comments Analysis.R
+++ BWRAO Comments Analysis.R
@@ -8,13 +8,12 @@ library(zoo)
 setwd("Personal Git/BWRAO-Comment-Scraper/")
 
 
-links1<-read.table(file="Data/archive_links_1_78.txt")
-links2<-read.table(file="Data/archive_links_79_97.txt")
-links3<-read.table(file="Data/archive_links_98_end.txt")
-
-links_orig <-rbind(links1,links2,links3) 
-
+#links1<-read.table(file="Data/archive_links_1_78.txt")
+#links2<-read.table(file="Data/archive_links_79_97.txt")
+#links3<-read.table(file="Data/archive_links_98_end.txt")
+#links_orig <-rbind(links1,links2,links3) 
 #write.csv(links_orig, "Data/links_orig.csv",row.names=F)
+
 
 comment_files<-list.files(path="Data/",pattern = ".csv")
 
@@ -27,15 +26,30 @@ for(j in 1:length(comment_files)){
                                 ,"Comment_Poster","Comment_Date","Comment_Time","Comment_Recs")
 } 
 
-comments <- bind_rows(comment_list)
+comments1 <- bind_rows(comment_list[1:2])
 
-#write.csv(comments, "Data/comments_orig.csv",row.names=F)
+c1<-comments1[1:100000,]
+c2<-comments1[100001:200000,]
+c3<-comments1[200001:300000,]
+c4<-comments1[300001:400000,]
+c5<-comments1[400001:500000,]
+c6<-comments1[500001:630292,]
 
-users <- comments$Comment_Poster %>% unique()
+write.csv(c1, "Data/Comments1.csv",row.names=F)
+write.csv(c2, "Data/Comments2.csv",row.names=F)
+write.csv(c3, "Data/Comments3.csv",row.names=F)
+write.csv(c4, "Data/Comments4.csv",row.names=F)
+write.csv(c5, "Data/Comments5.csv",row.names=F)
+write.csv(c6, "Data/Comments6.csv",row.names=F)
+
+users <- comments1$Comment_Poster %>% unique()
 write.table(users,"Data/users.txt")
+
+
 
 ###############################
 # Formatting Dates
+comments <- comments1
 
 comments$Article.Publish.Date <- as.character(comments$Article.Publish.Date)
 comments$Article.Publish.Date <- anytime::anytime(comments$Article.Publish.Date)
@@ -50,11 +64,11 @@ comments$Comment.Date <- as.Date(comments$Comment.Date)
 
 comments$Comment.Body <- as.character(comments$Comment.Body)
 
-write.csv(comments,file = "Processed_comments.csv",row.names = F)
+
+###############################
+# Visualizations
 
 ## Comment count over time
-
-
 comments_time <- comments %>% group_by(Article.Publish.Date) %>% tally()
 comments_time$trailing_30_avg <- zoo::rollmeanr(comments_time$n,k=30,fill=NA)
 
@@ -66,5 +80,3 @@ time_chart<-ggplot(comments_time) +
   theme_minimal() + ylab("Comment Count") +
   ggtitle("Daily Count of Comments",subtitle = "30-day avg. in brown")
 
-# top commentors
-top_commentors <- comments %>% group_by(Comment.Poster) %>% tally() %>% arrange(-n) %>% mutate(Perc=n/nrow(comments)*100)
